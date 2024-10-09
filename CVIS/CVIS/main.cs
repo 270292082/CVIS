b74512b3880ccc792d2d3271b221aa36db7c2707
Thomas Alexis Wongsokarto <tomwongs.yoobee@gmail.com>
2024-10-10 11:51:38 +1300

    Update main.cs
    Reformat

--- CVIS/CVIS/main.cs
+++ CVIS/CVIS/main.cs
@@ -2,33 +2,63 @@
  * Classes to implement:
  *  
  *   Patient;
- *       name
- *       last_name
- *       id
- *       phone
- *       email
- *       vaccine_status
+ *     - name
+ *     - last_name
+ *     - id
+ *     - phone
+ *     - email
+ *     - vaccine_status
+ *
+ *     + getName()
+ *     + getLastName()
+ *     + getID()
+ *     + getPhone()
+ *     + getEmail()
+ *     + getVaccineStat()
  *      
  *   Staff;
- *       name
- *       last_name
- *       id
- *       phone
- *       email
+ *     - name
+ *     - last_name
+ *     - id
+ *     - phone
+ *     - email
+ *
+ *     + getName()
+ *     + getLastName()
+ *     + getID()
+ *     + getPhone()
+ *     + getEmail()
  *  
  *   Mod;
- *       name
- *       last_name
- *       id
- *       phone
- *       email
+ *     - name
+ *     - last_name
+ *     - id
+ *     - phone
+ *     - email
+ *
+ *     + getName()
+ *     + getLastName()
+ *     + getID()
+ *     + getPhone()
+ *     + getEmail()
  *      
  *   Admin;
- *       name
- *       last_name
- *       id
- *       phone
- *       email
+ *     - name
+ *     - last_name
+ *     - id
+ *     - phone
+ *     - email
+ *
+ *     + getName()
+ *     + getLastName()
+ *     + getID()
+ *     + getPhone()
+ *     + getEmail()
+ *
+ *     + access_control() ; Launch the users' access control if logged as admin.
+ *     + system_update() ; Launch an update of the system if logged as admin.
+ *     + monitor_system() ; Launch the system monitor if logged as admin.
+ *     + backup_dataset() ; Make a backup of the dataset if logged as admin.
  *      
  *  
  * Functions to implement:
@@ -55,12 +85,7 @@
  *   getAdminInfo() ; No input and output a dictionary of strings.
  *   setAdminInfo(var dict) ; Receive a dictionary as input and redefine the dataset with this list.
  *  
- *  
- *   access_control() ; Launch the users' access control if logged as admin.
- *   system_update() ; Launch an update of the system if logged as admin.
- *   monitor_system() ; Launch the system monitor if logged as admin.
- *   backup_dataset() ; Make a backup of the dataset if logged as admin.
- *  
+ * 
  *  
  *   report_issue() ; Launch the issue report page.
  *   reset_passwd() ; Launch the password reset page for the logged user.
