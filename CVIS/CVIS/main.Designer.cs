a80fbf6e084baa667d96eccbc4f120f483142e60
Thomas Alexis Wongsokarto <tomwongs.yoobee@gmail.com>
2024-10-15 08:44:10 +1200

    Refactor
    I forgot this change, oops

--- CVIS/CVIS/main.Designer.cs
+++ CVIS/CVIS/main.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(202, 207);
             this.Controls.Add(this.qrcode);
             this.Name = "index";
             this.Text = "CVIS";
