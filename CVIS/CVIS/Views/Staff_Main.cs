41bcbacba79b98e1722a87ff98468c8a383d0fe5
Thomas Alexis Wongsokarto <tomwongs.yoobee@gmail.com>
2024-10-31 11:25:19 -1100

    Glabolga
    More implementation and feature for the staff main page and a few bug fixing.

--- CVIS/CVIS/Views/Staff_Main.cs
+++ CVIS/CVIS/Views/Staff_Main.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using SysFunc;
+
 namespace CVIS
 {
     public partial class Staff_Main : Form
@@ -21,6 +23,44 @@ namespace CVIS
             welcome_text.Text = "Welcome, ";
             welcome_text.Text += staff.FirstName + " ";
             welcome_text.Text += staff.LastName + "!";
+
+            daily_report_info.Text = "Date: ";
+            daily_report_info.Text += "" + "\n"; // Insert the data.
+            daily_report_info.Text += "Total Vaccinated: " + "\n";
+            daily_report_info.Text += ""; // Insert the data.
+
+            montly_report_info.Text = "Month: ";
+            montly_report_info.Text += "" + "\n"; // Insert the data.
+            montly_report_info.Text += "Total Vaccinated: " + "\n";
+            montly_report_info.Text += "" + "\n"; // Insert the data.
+
+            yearly_report_info.Text = "Year: ";
+            yearly_report_info.Text += "" + "\n"; // Insert the data.
+            yearly_report_info.Text += "Total Vaccinated: " + "\n";
+            yearly_report_info.Text += "" + "\n"; // Insert the data.
+
+
+            // Set the position and size for the navigation panel.
+            display_nav.Size = new Size(251, 361);
+            display_nav.Location = new Point(-251, 0);
+            display_nav.Visible = true;
+            Sys.loadPage(display_nav, new Navigation(_display, display_nav));
+
+        }
+
+        private void scan_qr_button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("No scanners were detected!", "ERROR!");
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menu_button_Click(object sender, EventArgs e)
+        {
+            Sys.toggleNav(display_nav, 1);
         }
     }
 }
